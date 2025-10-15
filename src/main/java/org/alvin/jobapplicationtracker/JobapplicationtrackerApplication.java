c46d490216ac8b924eb599940ce7eb702cbb3544
st-vin <onsongoalvin@gmail.com>
2025-10-15 23:51:10 +0300

    project setup

---
+++ src/main/java/org/alvin/jobapplicationtracker/JobapplicationtrackerApplication.java
@@ -0,0 +1,13 @@
+package org.alvin.jobapplicationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobapplicationtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobapplicationtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alvin/jobapplicationtracker/JobapplicationtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alvin.jobapplicationtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobapplicationtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
