7d4ec87e33d792962bed8a30a2b730df5a376df6
st-vin <onsongoalvin@gmail.com>
2025-10-18 20:52:58 +0300

    Define standardized enum values for application lifecycle states

--- src/main/java/org/alvin/jobapplicationtracker/entity/ApplicationStatus.java
+++ src/main/java/org/alvin/jobapplicationtracker/entity/ApplicationStatus.java
@@ -1,4 +1,11 @@
 package org.alvin.jobapplicationtracker.entity;
 
 public enum ApplicationStatus {
+        APPLIED,
+        PHONE_SCREEN,
+        TECHNICAL,
+        ONSITE,
+        OFFER,
+        REJECTED,
+        WITHDRAWN
 }
