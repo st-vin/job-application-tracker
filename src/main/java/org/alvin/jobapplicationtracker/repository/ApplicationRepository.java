74fc2ab9f6d31316b0d9ddb14a09bed8ac9808a6
st-vin <onsongoalvin@gmail.com>
2025-10-18 20:54:35 +0300

    Refine ApplicationRepository interface for CRUD operations and entity access

--- src/main/java/org/alvin/jobapplicationtracker/repository/ApplicationRepository.java
+++ src/main/java/org/alvin/jobapplicationtracker/repository/ApplicationRepository.java
@@ -1,4 +1,7 @@
 package org.alvin.jobapplicationtracker.repository;
 
-public interface ApplicationRepository {
+import org.alvin.jobapplicationtracker.entity.ApplicationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationRepository extends JpaRepository<ApplicationEntity, Long> {
 }
