1ff1b1b7b59071169fe67b9a7a856da38e86deb4
st-vin <onsongoalvin@gmail.com>
2025-10-18 20:55:01 +0300

    Implement UserRepository interface for user persistence layer

--- src/main/java/org/alvin/jobapplicationtracker/repository/UserRepository.java
+++ src/main/java/org/alvin/jobapplicationtracker/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package org.alvin.jobapplicationtracker.repository;
 
-public interface UserRepository {
+import org.alvin.jobapplicationtracker.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long>{
+
 }
