6c090be7ebca8b5a30aee47d8332abe41d5bb6f1
st-vin <onsongoalvin@gmail.com>
2025-10-20 22:57:30 +0300

    Extend ApplicationRepository with query methods for application filtering

--- src/main/java/org/alvin/jobapplicationtracker/repository/ApplicationRepository.java
+++ src/main/java/org/alvin/jobapplicationtracker/repository/ApplicationRepository.java
@@ -1,7 +1,27 @@
 package org.alvin.jobapplicationtracker.repository;
 
 import org.alvin.jobapplicationtracker.entity.ApplicationEntity;
+import org.alvin.jobapplicationtracker.entity.ApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface ApplicationRepository extends JpaRepository<ApplicationEntity, Long> {
+
+/*    // find applications by id
+    Optional<ApplicationEntity> findById(Long id);
+    // find applications by status
+    List<ApplicationEntity> findByStatus(ApplicationStatus status);
+
+    // find applications by company name
+    List<ApplicationEntity> findByCompanyName(String companyName);
+
+   // sort applications by salary descending
+    List<ApplicationEntity> OrderBySalaryDesc(Double salary);
+
+    // sort applications by salary ascending
+    List<ApplicationEntity> OrderBySalaryAsc(Double salary);*/
 }
