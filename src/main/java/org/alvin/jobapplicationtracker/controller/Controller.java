499249b340b57d8cc2970e2abfa3684e3441309c
st-vin <onsongoalvin@gmail.com>
2025-10-25 15:35:16 +0300

    Improve Controller logic and request mappings for cleaner API handling

--- src/main/java/org/alvin/jobapplicationtracker/controller/Controller.java
+++ src/main/java/org/alvin/jobapplicationtracker/controller/Controller.java
@@ -1,4 +1,23 @@
 package org.alvin.jobapplicationtracker.controller;
 
+import org.alvin.jobapplicationtracker.entity.ApplicationEntity;
+import org.alvin.jobapplicationtracker.repository.ApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class Controller {
+    @Autowired
+    public ApplicationRepository applicationRepository;
+
+
+    @GetMapping("/applications/{id}")
+    public Optional<ApplicationEntity> getApplicationByIdWithUser(@PathVariable("id") Long id) {
+        return applicationRepository.findByIdWithUser(id);
+    }
 }
