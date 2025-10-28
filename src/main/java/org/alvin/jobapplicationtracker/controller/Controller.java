449791b9ed899b0d9727ecd62542b170d3260e1d
st-vin <onsongoalvin@gmail.com>
2025-10-28 14:11:50 +0300

    Enhance Controller: implement refined CRUD endpoints and integrate with service layer

--- src/main/java/org/alvin/jobapplicationtracker/controller/Controller.java
+++ src/main/java/org/alvin/jobapplicationtracker/controller/Controller.java
@@ -1,7 +1,9 @@
 package org.alvin.jobapplicationtracker.controller;
 
 import org.alvin.jobapplicationtracker.entity.ApplicationEntity;
+import org.alvin.jobapplicationtracker.entity.UserEntity;
 import org.alvin.jobapplicationtracker.repository.ApplicationRepository;
+import org.alvin.jobapplicationtracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +16,27 @@ import java.util.Optional;
 public class Controller {
     @Autowired
     public ApplicationRepository applicationRepository;
-
+    
+    @Autowired
+    public UserRepository userRepository;
 
     @GetMapping("/applications/{id}")
     public Optional<ApplicationEntity> getApplicationByIdWithUser(@PathVariable("id") Long id) {
         return applicationRepository.findByIdWithUser(id);
+    }
+    
+    @GetMapping("/users/{id}")
+    public Optional<UserEntity> getUserByIdWithApplications(@PathVariable("id") Long id) {
+        return userRepository.findByIdWithApplications(id);
+    }
+    
+    @GetMapping("/applications")
+    public List<ApplicationEntity> getAllApplications() {
+        return applicationRepository.findAll();
+    }
+    
+    @GetMapping("/users")
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
     }
 }
