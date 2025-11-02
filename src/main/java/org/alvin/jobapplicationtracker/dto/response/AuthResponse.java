51676de54005136d58cc504badcf821d7d5b54bd
st-vin <onsongoalvin@gmail.com>
2025-11-02 21:17:56 +0300

    refactor(dto): modify authentication response payload

--- src/main/java/org/alvin/jobapplicationtracker/dto/response/AuthResponse.java
+++ src/main/java/org/alvin/jobapplicationtracker/dto/response/AuthResponse.java
@@ -1,4 +1,16 @@
 package org.alvin.jobapplicationtracker.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
+    private String token;
+    private String type = "Bearer"; // Token type
+    private UserResponseDTO user;
 }
