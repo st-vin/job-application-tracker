5b41cb7809b9476a07d193c5c8e4d44f9878792f
st-vin <onsongoalvin@gmail.com>
2025-11-02 21:19:18 +0300

    refactor(service): adjust authentication service interface

--- src/main/java/org/alvin/jobapplicationtracker/service/AuthService.java
+++ src/main/java/org/alvin/jobapplicationtracker/service/AuthService.java
@@ -1,4 +1,9 @@
+// service/AuthService.java
 package org.alvin.jobapplicationtracker.service;
 
+import org.alvin.jobapplicationtracker.dto.request.LoginRequest;
+import org.alvin.jobapplicationtracker.dto.response.AuthResponse;
+
 public interface AuthService {
+    AuthResponse login(LoginRequest request);
 }
